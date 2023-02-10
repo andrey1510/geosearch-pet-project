9c6d71ad7bbdb1f8993348aa066b719dcf365d61
andrey-1510 <andrey-1510@outlook.com>
2023-02-10 18:17:00 +0300

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4Mzg1Ng/spectral.js?branch_slug=main&token=72976de7-ecd7-47db-be37-abc225bcbdef";
+export default { extends: ruleset };
