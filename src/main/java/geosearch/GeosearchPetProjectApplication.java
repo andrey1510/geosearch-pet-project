d4328e91d37f2dd8da098d3932d326251ad8d532
andrey1510 <andrey-1510@outlook.com>
2023-02-10 18:02:07 +0300

    initial commit

---
+++ src/main/java/geosearch/GeosearchPetProjectApplication.java
@@ -0,0 +1,13 @@
+package geosearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeosearchPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeosearchPetProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/geosearch/GeosearchPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package geosearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeosearchPetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
